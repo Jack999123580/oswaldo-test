07dab036aeddc5de7dc54e3e2e133a44f48220a1
ivan <ivan@gmail.com>
2026-08-29 14:27:06 +0300

    add arrow

--- src/sections/Welcome/PathToMoney/PathToMoney.tsx
+++ src/sections/Welcome/PathToMoney/PathToMoney.tsx
@@ -1,4 +1,5 @@
 import { ActionButtons } from './ActionButtons'
+import arrowIcon from './assets/arrow.svg'
 import bitcoinIcon from './assets/bitcoin.svg'
 import catIcon from './assets/cat.svg'
 import styles from './PathToMoney.module.less'
@@ -10,6 +11,7 @@ export const PathToMoney = () => {
             <div className={styles.headline}>
                 <img src={catIcon} className={styles.cat} />
                 <Title />
+                <img src={arrowIcon} className={styles.arrow} alt="" />
                 <div className={styles.bitcoin}>
                     <img src={bitcoinIcon} alt="" />
                 </div>
