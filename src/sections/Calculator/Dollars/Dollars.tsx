8d10c06158fd44300f40cd853ec23c1db7abab79
ivan <ivan@gmail.com>
2026-08-30 15:40:07 +0300

    refactoring

---
+++ src/sections/Calculator/Dollars/Dollars.tsx
@@ -0,0 +1,37 @@
+import cn from 'classnames'
+import type { FC } from 'react'
+
+import { CircleGradient } from '../../../components/CircleGradient'
+import { formatNumberWithSpaces } from '../../../utils'
+import dollarImage from './assets/dollar.svg'
+import leafCenter from './assets/leafCenter.svg'
+import leafFront from './assets/leafFront.svg'
+import leafLeftBottom from './assets/leafLeftBottom.svg'
+import leafMiddle from './assets/leafMiddle.svg'
+import leafTopRight from './assets/leafTopRight.svg'
+import styles from './Dollars.module.less'
+
+type DollarsProps = {
+    income: number
+}
+
+export const Dollars: FC<DollarsProps> = ({ income }) => {
+    return (
+        <div className={styles.result}>
+            <CircleGradient blur={80} className={styles.glowFront} />
+            <img src={leafTopRight} alt="" className={styles.leafTopRight} />
+            <img src={leafCenter} alt="" className={styles.leafCenter} />
+            <img src={leafLeftBottom} alt="" className={styles.leafLeftBottom} />
+            <img src={leafMiddle} alt="" className={styles.leafMiddle} />
+            <img src={leafFront} alt="" className={styles.leafFront} />
+            <img src={dollarImage} alt="" className={styles.dollar} />
+            <div className={styles.glowContainer}>
+                <CircleGradient blur={80} className={styles.glow} />
+            </div>
+            <p className={cn(styles.resultLabel, 'mb-0')}>Ты заработаешь</p>
+            <p className={cn(styles.resultValue, 'fs-2 fs-lg-1 mb-0')}>
+                {formatNumberWithSpaces(income)} ₽
+            </p>
+        </div>
+    )
+}
---
+++ src/sections/Calculator/Dollars/index.ts
@@ -0,0 +1,1 @@
+export { Dollars } from './Dollars'
